e851d6719b24fe522982e077c4feae9986e65744
Ashley Valent <ashley.valent@gmail.com>
2016-12-11 22:15:07 -0800

    Added nixpkgs config file.

---
+++ nixpkgs/config.nix
@@ -0,0 +1,12 @@
+# From https://nixos.org/wiki/Cheatsheet#config_nix
+{
+  packageOverrides = pkgs_: with pkgs_; {
+    all = with pkgs; buildEnv {
+      name = "all";
+      paths = [
+        emacs
+        file
+      ];
+    };
+  };
+}
