391c99e769a2de35283dbed0eeb545cf9af75540
Ashley Valent <ashley.valent@gmail.com>
2018-02-06 19:00:43 -0800

    Very basic xmonad configuration for xfce.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,12 +1,22 @@
- --
+--
 -- An example, simple ~/.xmonad/xmonad.hs file.
 -- It overrides a few basic settings, reusing all the other defaults.
 --
 
 import XMonad
+import XMonad.Config.Kde
+import XMonad.Config.Xfce
 
-main = xmonad $ def
-    { borderWidth        = 2
-    , terminal           = "urxvt"
-    , normalBorderColor  = "#cccccc"
-    , focusedBorderColor = "#cd8b00" }
+{-main = xmonad kdeConfig-}
+main = xmonad xfceConfig
+                { borderWidth        = 2
+                , terminal           = "urxvt"
+                , normalBorderColor  = "#cccccc"
+                , focusedBorderColor = "#cd8b00"
+                }
+
+--main = xmonad $ def
+--    { borderWidth        = 2
+--    , terminal           = "urxvt"
+--    , normalBorderColor  = "#cccccc"
+--    , focusedBorderColor = "#cd8b00" }
